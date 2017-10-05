5aded7cbc1b106e85f4e7929521c298100974303
tripott <tripott@gmail.com>
2017-10-05 15:50:28 -0400

    error handling with node-http-error

--- app.js
+++ app.js
@@ -3,14 +3,28 @@ const port = process.env.PORT || 4000
 const dal = require('./dal.js')
 const express = require('express')
 const app = express()
+const HTTPError = require('node-http-error')
 
 // get a book   GET /books/id
-
 app.get('/books/:id', function(req, res, next) {
   dal.getBook(req.params.id, function(err, doc) {
-    if (err) return console.log(err)
+    if (err)
+      return next(
+        new HTTPError(err.status, err.message, {
+          description:
+            'That doc couldnt be found. You might want to check the path for errors.'
+        })
+      )
     res.status(200).send(doc)
   })
+})
+
+////////////////////////
+//  ERROR HANDLER
+///////////////////////
+app.use(function(err, req, res, next) {
+  console.log(req.method, ' ', req.path, ' ', 'error ', err)
+  res.status(err.status || 500).send(err)
 })
 
 app.listen(port, () => console.log('API is up on port', port))
