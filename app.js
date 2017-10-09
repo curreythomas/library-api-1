d2b6780b93f27251581c973a7a11e98fe9d163f7
CamIAm <rcmontgo@gmail.com>
2017-10-09 13:51:18 -0400

    put method in process

--- app.js
+++ app.js
@@ -69,7 +69,19 @@ app.get('/authors/:id', (req, res, next) =>
   })
 )
 
-//app.put('/authors/:id', (req, res, next) => {})
+// app.put('/authors/:id', (req, res, next) => {
+//   if(isEmpty(prop('body'),req)){
+//     return next(new HTTPError(400,'Missing request body.  Content-Type header should be application/json.'))
+//   }
+//   const missingFields = checkRequiredFields(['_id','_rev','placeOfBirth','birthDate','name'],prop('body',req))
+//   if(not(isEmpty(missingFields))){
+//     return next(new HTTPError(401,`Missing required fields: ${join(' ',missingFields)}`))
+//   }
+//   updateAuthor(prop('body',req),(err,result)=>{
+//     if(err)
+//   })
+//
+// })
 
 app.post('/books', function(req, res, next) {
   // check to make sure the request body exists
